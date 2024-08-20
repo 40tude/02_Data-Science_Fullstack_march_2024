55de46b58d058b58fd6119f586d7a1e7a39bcfdc
Philippe BAUCOUR <philippe.baucour@gmail.com>
2024-08-20 15:35:28 +0200

    slides kayak et relecture code

--- 03_data_collection/99_Project_Kayak/scraper8_attributes.py
+++ 03_data_collection/99_Project_Kayak/scraper8_attributes.py
@@ -2,6 +2,7 @@
 # genère un fichier hotels_attributes avec une ligne d'attirbuts par hotel
 
 import json
+import re
 from pathlib import Path
 import logging
 import scrapy
@@ -17,8 +18,8 @@ k_CurrentDir = Path(__file__).parent
 class OneHotelSpider(scrapy.Spider):
     name = "OneHotelSpider"
     # make the url are parsed in order
-    # however we loose the // ant it is sloooow
-    # 1 min to get the attributs for 25 hotels
+    # however we loose the // and it is sloooow
+    # 1 min to get the attributes for 25 hotels
     # custom_settings = {
     #   'CONCURRENT_REQUESTS': '1'
     # }
@@ -42,26 +43,25 @@ class OneHotelSpider(scrapy.Spider):
         rank = response.meta.get("rank")
 
         # TODO this guys need a review. Indeed, I'm not these hard coded selectors will survive a long time
-        # TODO At least they work fine 07/04/2024
+        # 07 04 2024
         # kScoreSelector = ".a3b8729ab1.d86cee9b25::text"
         # kDescription2Selector = (
         #     ".a53cbfa6de.b3efd73f69::text"  # <p data-testid="property-description" class="a53cbfa6de b3efd73f69">....
         # )
-        # klatlng = "div.hotel-sidebar-map hotel-sidebar-map-a11y a::attr(href)"  # <div class="hotel-sidebar-map hotel-sidebar-map-a11y">....
+        # kLatLng = "div.hotel-sidebar-map hotel-sidebar-map-a11y a::attr(href)"  # <div class="hotel-sidebar-map hotel-sidebar-map-a11y">....
 
+        # 19 08 2024
+        # Check assets/kScoreSelector.png, kDescription2Selector.png and kLatLng.png to see from where the values below come from
         kScoreSelector = ".d0522b0cca.fd44f541d8::text"
         kDescription2Selector = ".e2585683de.c8d1788c8c::text"
-        # klatlng = "div.hotel-sidebar-map hotel-sidebar-map-a11y a::attr(href)"  # <div class="hotel-sidebar-map hotel-sidebar-map-a11y">....
+        kLatLng = "a[data-atlas-latlng]::attr(data-atlas-latlng)"
 
         score = response.css(kScoreSelector).get().replace(",", ".")
         description = response.css(kDescription2Selector).get()
 
-        # url = response.css("div.hotel-sidebar-map a::attr(data-atlas-latlng)").extract()
-        # <div class="b95907b7ce" style="background-image: url(&quot;https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBsCRN3eiZNxm0nbI9snuHn3_shiZ3I6zQ&amp;channel=hotel&amp;center=48.8551725227304%2C2.36022859811783&amp;size=261x182&amp;zoom=13&amp;signature=Xxfoh14CsX8ZR2ZatU_vjGJ_ZH4=&quot;);"><div class="f96723525b"><div class="d3f3aeebf0" data-testid="map-entry-point-marker"><span class="d3a235084b" data-testid="map-entry-pin-icon"><span class="b2142222c3"><span class="b235b4f8e2"></span><svg viewBox="-1 -1 26 32" class="af04624a2c"><path d="M24 12.4286C24 19.2927 12 29 12 29C12 29 0 19.2927 0 12.4286C0 5.56446 5.37258 0 12 0C18.6274 0 24 5.56446 24 12.4286Z"></path></svg></span><svg class="cd891e017f" viewBox="0 0 12 4"><ellipse cx="6" cy="2" rx="6" ry="2"></ellipse></svg></span></div><button data-map-trigger-button="1" type="button" class="dba1b3bddf e99c25fd33 f8a5a77b19 a86bcdb87f bec09c39da bbe6fc3743"><span class="eed450ee2f">Voir sur la carte</span></button></div></div>
-        url = response.css(".b95907b7ce").get()
-        # hotel_sidebar_static_map_capla > div > div
-        latitude = url[0].split(",")[0]
-        longitude = url[0].split(",")[1]
+        # valeur de l'attribut 'data-atlas-latlng'
+        latlng = response.css(kLatLng).get()
+        latitude, longitude = latlng.split(",")
         # print(latitude, longitude)
 
         processed_data = {
