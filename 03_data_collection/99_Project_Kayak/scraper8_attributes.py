2d42355fb6acc5dc5f9dd67b0b4284dd9979b47d
Philippe BAUCOUR <philippe.baucour@gmail.com>
2024-08-20 07:52:13 +0200

    Relecture projet Kayak

--- 03_data_collection/99_Project_Kayak/scraper8_attributes.py
+++ 03_data_collection/99_Project_Kayak/scraper8_attributes.py
@@ -8,8 +8,8 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 
 import include_kayak as k
-k_CurrentDir     = Path(__file__).parent
 
+k_CurrentDir = Path(__file__).parent
 
 
 # -----------------------------------------------------------------------------
@@ -17,87 +17,92 @@ k_CurrentDir     = Path(__file__).parent
 class OneHotelSpider(scrapy.Spider):
     name = "OneHotelSpider"
     # make the url are parsed in order
-    # however we loose the // ant it is sloooow 
+    # however we loose the // ant it is sloooow
     # 1 min to get the attributs for 25 hotels
     # custom_settings = {
-    #   'CONCURRENT_REQUESTS': '1' 
+    #   'CONCURRENT_REQUESTS': '1'
     # }
 
     def start_requests(self):
 
-      try:
-        with open(k_CurrentDir/k.AssetsDir/k.HotelsListFile, 'r') as f:
-          data = json.load(f)
-      except FileNotFoundError:
-        print(f'Are you sure the file {k.HotelsListFile} exists ?')
+        try:
+            with open(k_CurrentDir / k.AssetsDir / k.HotelsListFile, "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            print(f"Are you sure the file {k.HotelsListFile} exists ?")
 
-      hotel_rank = 0
-      for entry in data:
-        yield scrapy.Request(url=entry['url'], callback=self.parse, meta={"rank": hotel_rank})
-        hotel_rank += 1
-        
-
+        hotel_rank = 0
+        for entry in data:
+            yield scrapy.Request(url=entry["url"], callback=self.parse, meta={"rank": hotel_rank})
+            hotel_rank += 1
 
     def parse(self, response):
-      
-      # get back the rank of the hotel
-      rank = response.meta.get("rank")
 
-      # TODO this guys need a review. Indeed, I'm not these hard coded selectors will survive a long time
-      # TODO At least they work fine 07/04/2024 
-      kScoreSelector          = '.a3b8729ab1.d86cee9b25::text'
-      # kDescription1Selector = 'property_description_content::text'                            # <div id="property_description_content" ...
-      kDescription2Selector   = '.a53cbfa6de.b3efd73f69::text'                                  # <p data-testid="property-description" class="a53cbfa6de b3efd73f69">....
-      klatlng                 = "div.hotel-sidebar-map hotel-sidebar-map-a11y a::attr(href)"    # <div class="hotel-sidebar-map hotel-sidebar-map-a11y">....
+        # get back the rank of the hotel
+        rank = response.meta.get("rank")
 
-      score = response.css(kScoreSelector).get().replace(",", ".")  
-      
-      url = response.css("div.hotel-sidebar-map a::attr(data-atlas-latlng)").extract()
-      latitude = url[0].split(",")[0]
-      longitude = url[0].split(",")[1]
-      # print(latitude, longitude)
+        # TODO this guys need a review. Indeed, I'm not these hard coded selectors will survive a long time
+        # TODO At least they work fine 07/04/2024
+        # kScoreSelector = ".a3b8729ab1.d86cee9b25::text"
+        # kDescription2Selector = (
+        #     ".a53cbfa6de.b3efd73f69::text"  # <p data-testid="property-description" class="a53cbfa6de b3efd73f69">....
+        # )
+        # klatlng = "div.hotel-sidebar-map hotel-sidebar-map-a11y a::attr(href)"  # <div class="hotel-sidebar-map hotel-sidebar-map-a11y">....
 
-      description = response.css(kDescription2Selector).get()
+        kScoreSelector = ".d0522b0cca.fd44f541d8::text"
+        kDescription2Selector = ".e2585683de.c8d1788c8c::text"
+        # klatlng = "div.hotel-sidebar-map hotel-sidebar-map-a11y a::attr(href)"  # <div class="hotel-sidebar-map hotel-sidebar-map-a11y">....
 
-      processed_data = {
-        "rank"        : rank,
-        "score"       : score,
-        
-        "latitude"    : latitude,
-        "longitude"   : longitude,
-        "description" : description,
-      }
-      yield processed_data
+        score = response.css(kScoreSelector).get().replace(",", ".")
+        description = response.css(kDescription2Selector).get()
 
+        # url = response.css("div.hotel-sidebar-map a::attr(data-atlas-latlng)").extract()
+        # <div class="b95907b7ce" style="background-image: url(&quot;https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBsCRN3eiZNxm0nbI9snuHn3_shiZ3I6zQ&amp;channel=hotel&amp;center=48.8551725227304%2C2.36022859811783&amp;size=261x182&amp;zoom=13&amp;signature=Xxfoh14CsX8ZR2ZatU_vjGJ_ZH4=&quot;);"><div class="f96723525b"><div class="d3f3aeebf0" data-testid="map-entry-point-marker"><span class="d3a235084b" data-testid="map-entry-pin-icon"><span class="b2142222c3"><span class="b235b4f8e2"></span><svg viewBox="-1 -1 26 32" class="af04624a2c"><path d="M24 12.4286C24 19.2927 12 29 12 29C12 29 0 19.2927 0 12.4286C0 5.56446 5.37258 0 12 0C18.6274 0 24 5.56446 24 12.4286Z"></path></svg></span><svg class="cd891e017f" viewBox="0 0 12 4"><ellipse cx="6" cy="2" rx="6" ry="2"></ellipse></svg></span></div><button data-map-trigger-button="1" type="button" class="dba1b3bddf e99c25fd33 f8a5a77b19 a86bcdb87f bec09c39da bbe6fc3743"><span class="eed450ee2f">Voir sur la carte</span></button></div></div>
+        url = response.css(".b95907b7ce").get()
+        # hotel_sidebar_static_map_capla > div > div
+        latitude = url[0].split(",")[0]
+        longitude = url[0].split(",")[1]
+        # print(latitude, longitude)
 
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-# If files exist, delete them 
-if Path.exists(k_CurrentDir/k.AssetsDir/k.HotelsAttributes):
-  (k_CurrentDir/k.AssetsDir/k.HotelsAttributes).unlink()
-if Path.exists(k_CurrentDir/k.AssetsDir/k.ScrapyLogFile):
-  (k_CurrentDir/k.AssetsDir/k.ScrapyLogFile).unlink()
-
-process = CrawlerProcess(
-    settings={
-        "USER_AGENT": "Chrome/97.0",
-        "LOG_LEVEL": logging.INFO,  # CRITICAL, ERROR, WARNING, INFO, DEBUG...
-        "FEEDS": {
-          k_CurrentDir/k.AssetsDir/k.HotelsAttributes: {"format": "json"},  
-        },
-        "LOG_STDOUT": False,
-        "LOG_FILE": f"{k_CurrentDir}/{k.AssetsDir}/{k.ScrapyLogFile}",
-    }
-)
-
-
+        processed_data = {
+            "rank": rank,
+            "score": score,
+            "latitude": latitude,
+            "longitude": longitude,
+            "description": description,
+        }
+        yield processed_data
 
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
 
-  process.crawl(OneHotelSpider)
-  process.start()
+    # If it exists delete previous version of scrapy log file
+    log_file_path = k_CurrentDir / k.AssetsDir / k.ScrapyLogFile
+    if log_file_path.exists():
+        log_file_path.unlink()
+    # if Path.exists(k_CurrentDir / k.AssetsDir / k.ScrapyLogFile):
+    #     (k_CurrentDir / k.AssetsDir / k.ScrapyLogFile).unlink()
 
+    # If it exists delete previous version of attributes attributes file
+    attr_file_path = k_CurrentDir / k.AssetsDir / k.HotelsAttributes
+    if attr_file_path.exists():
+        attr_file_path.unlink()
+    # if Path.exists(k_CurrentDir / k.AssetsDir / k.HotelsAttributes):
+    #       (k_CurrentDir / k.AssetsDir / k.HotelsAttributes).unlink()
 
+    process = CrawlerProcess(
+        settings={
+            "USER_AGENT": "Chrome/97.0",
+            "LOG_LEVEL": logging.INFO,  # CRITICAL, ERROR, WARNING, INFO, DEBUG...
+            "FEEDS": {
+                k_CurrentDir / k.AssetsDir / k.HotelsAttributes: {"format": "json"},
+            },
+            "LOG_STDOUT": False,
+            "LOG_FILE": f"{k_CurrentDir}/{k.AssetsDir}/{k.ScrapyLogFile}",
+        }
+    )
+
+    process.crawl(OneHotelSpider)
+    process.start()
--- 03_data_collection/99_Project_Kayak/scraper9_hotels_per_city.py
+++ 03_data_collection/99_Project_Kayak/scraper9_hotels_per_city.py
@@ -11,9 +11,10 @@ from scrapy.crawler import CrawlerProcess
 from datetime import datetime, timedelta
 
 import include_kayak as k
-k_CurrentDir    = Path(__file__).parent
-k_CityName      = ""
-k_LenStay       = 3
+
+k_CurrentDir = Path(__file__).parent
+k_CityName = ""
+k_LenStay = 3
 k_InHowManyDays = 15
 
 
@@ -21,110 +22,134 @@ k_InHowManyDays = 15
 # -----------------------------------------------------------------------------
 def build_url(city_str):
 
-  # typiquement
-  # checkin  = today + 15 jours
-  # checkout = today + 15 jours + 3 jours
-  aujourdhui = datetime.today()
-  checkin = aujourdhui + timedelta(days=k_InHowManyDays)
-  checkin_str = checkin.strftime('%Y-%m-%d')
+    # typiquement
+    # checkin  = today + 15 jours
+    # checkout = today + 15 jours + 3 jours
+    aujourdhui = datetime.today()
+    checkin = aujourdhui + timedelta(days=k_InHowManyDays)
+    checkin_str = checkin.strftime("%Y-%m-%d")
 
-  checkout = checkin + timedelta(days=k_LenStay)
-  checkout_str = checkout.strftime('%Y-%m-%d')
+    checkout = checkin + timedelta(days=k_LenStay)
+    checkout_str = checkout.strftime("%Y-%m-%d")
 
-  return f"https://www.booking.com/searchresults.fr.html?ss={city_str}&checkin={checkin_str}&checkout={checkout_str}&group_adults=2&no_rooms=1&group_children=0"
+    # https://www.booking.com/searchresults.fr.html?ss=Paris&checkin=2024-09-03&checkout=2024-09-06&group_adults=2&no_rooms=1&group_children=0
+    return f"https://www.booking.com/searchresults.fr.html?ss={city_str}&checkin={checkin_str}&checkout={checkout_str}&group_adults=2&no_rooms=1&group_children=0"
 
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 class HotelsListSpider(scrapy.Spider):
-  
-  name = "HotelsListSpider"
 
-  
-  def start_requests(self):
+    name = "HotelsListSpider"
 
-    # try:
-    #   with open(kCurrentDir/kAssetsDir/kOneCityFile, 'r') as f:
-    #     data = json.load(f)
-    # except FileNotFoundError:
-    #   print(f"Are you sure the file {kOneCityFile} exists ?")
-    
-    # # Not yet usefull but we never know...
-    # # As today, it creates a list of url with only one url 
-    # urls = []
-    # for entry in data:
-    #   cityname = entry['city']
-    #   url = build_url(cityname)
-    #   urls.append(url)
-    
-    urls = []
-    url = build_url(k_CityName)
-    urls.append(url)
-    
-    for url in urls:
-      yield scrapy.Request(url=url, callback=self.parse)
-    
-    
+    def start_requests(self):
 
-  def parse(self, response):
-    # TODO this guys need a review. Indeed, I'm not these hard coded selectors will survive a long time
-    # TODO At least they work fine 07/04/2024 
-    kH3HotelSelector  = '.aab71f8e4e'
-    kNameSelector     = '.f6431b446c.a15b38c233::text'
-    kUrlSelector      = '.a78ca197d0::attr(href)'
+        # try:
+        #   with open(kCurrentDir/kAssetsDir/kOneCityFile, 'r') as f:
+        #     data = json.load(f)
+        # except FileNotFoundError:
+        #   print(f"Are you sure the file {kOneCityFile} exists ?")
 
-    counter = 0  
-    for hotel in response.css(kH3HotelSelector):
-      
-      counter +=1
-      if counter == 21:
-        break
+        # # Not yet usefull but we never know...
+        # # As today, it creates a list of url with only one url
+        # urls = []
+        # for entry in data:
+        #   cityname = entry['city']
+        #   url = build_url(cityname)
+        #   urls.append(url)
 
-      hotel_name = hotel.css(kNameSelector).extract_first()
-      url = hotel.css(kUrlSelector).get().split("?")[0]
+        urls = []
+        url = build_url(k_CityName)
+        urls.append(url)
 
-      processed_data = {
-        "hotel" : hotel_name,
-        "url"   : url,
-      }
-      yield processed_data
-    
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
+    def parse(self, response):
+        # TODO the constant guys need a review. Indeed, I'm not sure these hard coded selectors will survive a long time
+        # TODO At least they work fine 07/04/2024
+        # kH3HotelSelector = ".aab71f8e4e"
+        # kNameSelector = ".f6431b446c.a15b38c233::text"
+        # kUrlSelector = ".a78ca197d0::attr(href)"
 
+        # 19 08 2024
+        # Check assets/kH3HotelSelector.png, kNameSelector.png and kUrlSelector.png to see from where the values below come from
+        # kH3HotelSelector = ".b9687b0063"
 
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-# If files exist, delete them before the CrawlerProcess own it
-if Path.exists(k_CurrentDir/k.AssetsDir/k.ScrapyLogFile):
-  (k_CurrentDir/k.AssetsDir/k.ScrapyLogFile).unlink()
+        # bodyconstraint-inner > div:nth-child(8) > div > div.caa83e2398 > div.a84de7f213 > div.b9687b0063 > div.d830fa48ad.db402c28f2 > div.f9958fb57b > div:nth-child(3)                                                                            > div.d8ff70c6e0.bb80b6397f > div.c655c9a144 > div > div.adc8292e09.ea1e323a59.fffdb20d34.fbe4119cc7.fd229921e5 > div.c324bdcee4.ec7ca45eb7 > div > div:nth-child(1) > div            > h3 > a > div.e037993315.f5f8fe25fa
+        # bodyconstraint-inner > div:nth-child(8) > div > div.caa83e2398 > div.a84de7f213 > div.b9687b0063 > div.d830fa48ad.db402c28f2 > div.f9958fb57b > div.e01df12ddf.a0914461b0.d46a3604b5.ba1c6fdc7f.f550b7da28.b9a2fd8068.cb4a416743.b0cae6862c > div.d8ff70c6e0.bb80b6397f > div.c655c9a144 > div > div.adc8292e09.ea1e323a59.fffdb20d34.fbe4119cc7.fd229921e5 > div.c324bdcee4.ec7ca45eb7 > div > div:nth-child(1) > div.a8323349e9 > h3 > a > div.e037993315.f5f8fe25fa
+        # bodyconstraint-inner > div:nth-child(8) > div > div.caa83e2398 > div.a84de7f213 > div.b9687b0063 > div.d830fa48ad.db402c28f2 > div.f9958fb57b > div:nth-child(9) >                                                                            div.d8ff70c6e0.bb80b6397f > div.c655c9a144 > div > div.adc8292e09.ea1e323a59.fffdb20d34.fbe4119cc7.fd229921e5 > div.c324bdcee4.ec7ca45eb7 > div > div:nth-child(1) > div            > h3 > a > div.e037993315.f5f8fe25fa
+        kH3HotelSelector = ".d3e8e3d21a"
+        kNameSelector = ".e037993315.f5f8fe25fa::text"
+        kUrlSelector = ".f0ebe87f68::attr(href)"
 
-process = CrawlerProcess(
-    settings={
-        "USER_AGENT": "Chrome/97.0",
-        "LOG_LEVEL": logging.INFO,  # CRITICAL, ERROR, WARNING, INFO, DEBUG...
-        "FEEDS": {
-          k_CurrentDir/k.AssetsDir/k.HotelsListFile: {"format": "json"},  
-        },
-        "LOG_STDOUT": False,
-        "LOG_FILE": f"{k_CurrentDir}/{k.AssetsDir}/{k.ScrapyLogFile}",
-    }
-)
+        counter = 0
+        for hotel in response.css(kH3HotelSelector):
 
+            counter += 1
+            if counter == 21:
+                break
+
+            hotel_name = hotel.css(kNameSelector).extract_first()
+            url = hotel.css(kUrlSelector).get().split("?")[0]
+
+            processed_data = {
+                "hotel": hotel_name,
+                "url": url,
+            }
+            yield processed_data
 
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
 
-  # If files exist, delete it
-  if Path.exists(k_CurrentDir/k.AssetsDir/k.HotelsListFile):
-    (k_CurrentDir/k.AssetsDir/k.HotelsListFile).unlink()
+    if len(sys.argv) == 1:
+        # for testing/debug purpose
+        k_CityName = "Paris"
+    else:
+        k_CityName = sys.argv[1]
 
-  if len(sys.argv) == 1:
-    # For testing
-    k_CityName = "Paris"
-  else:  
-    k_CityName = sys.argv[1]
+    # if it exists delete previous version of scrapy log file
+    log_file_path = k_CurrentDir / k.AssetsDir / k.ScrapyLogFile
+    if log_file_path.exists():
+        log_file_path.unlink()
 
-  process.crawl(HotelsListSpider)
-  process.start()
+    # if it exists delete previous version of hotels_list.json
+    hotels_file_path = k_CurrentDir / k.AssetsDir / k.HotelsListFile
+    if hotels_file_path.exists():
+        hotels_file_path.unlink()
+
+    process = CrawlerProcess(
+        settings={
+            "USER_AGENT": "Chrome/97.0",
+            "LOG_LEVEL": logging.INFO,  # CRITICAL, ERROR, WARNING, INFO, DEBUG...
+            "FEEDS": {
+                k_CurrentDir / k.AssetsDir / k.HotelsListFile: {"format": "json"},
+            },
+            "LOG_STDOUT": False,
+            "LOG_FILE": k_CurrentDir / k.AssetsDir / k.ScrapyLogFile,
+        }
+    )
+
+    process.crawl(HotelsListSpider)
+    process.start()
+
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# If files exist, delete them before the CrawlerProcess own it
+# if Path.exists(k_CurrentDir / k.AssetsDir / k.ScrapyLogFile):
+#     (k_CurrentDir / k.AssetsDir / k.ScrapyLogFile).unlink()
+
+# process = CrawlerProcess(
+#     settings={
+#         "USER_AGENT": "Chrome/97.0",
+#         "LOG_LEVEL": logging.INFO,  # CRITICAL, ERROR, WARNING, INFO, DEBUG...
+#         "FEEDS": {
+#             k_CurrentDir / k.AssetsDir / k.HotelsListFile: {"format": "json"},
+#         },
+#         "LOG_STDOUT": False,
+#         "LOG_FILE": f"{k_CurrentDir}/{k.AssetsDir}/{k.ScrapyLogFile}",
+#     }
+# )
